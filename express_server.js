d0e503f0d1a405d29418b7641b5d9c679328d8e4
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-17 22:11:11 +0000

    added new form ejs file and randdom string function in server file

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +10,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//random 6 string generator
+const generateRandomString = () => Math.random().toString(36).substring(7);
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,7 +35,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
