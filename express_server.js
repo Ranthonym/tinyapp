6d277d44de7733fb471600b929535cf03e0a745d
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-17 18:17:41 +0000

    tested curl

--- express_server.js
+++ express_server.js
@@ -18,3 +18,7 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
