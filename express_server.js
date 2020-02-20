f8f4cd805c37ce866b4fa4f7853192a14b3c71d7
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-20 00:04:03 +0000

    redirects from new url page if not logged in

--- express_server.js
+++ express_server.js
@@ -69,7 +69,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {user:  users[req.cookies.user_id]};
+  if (req.cookies.user_id) {
   res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
