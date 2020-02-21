ab6c9c0794606ad750688f2ff5352bd63b233ec7
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-21 14:18:08 +0000

    refactored all helper functions to helpers.js & added README

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require('bcrypt');
-const  { getUserByEmail } = require('./helpers');
+const { getUserByEmail } = require('./helpers');
+const { generateRandomString } = require('./helpers');
+const { urlsForUser } = require('./helpers');
+
+//app config
 const app = express();
 const PORT = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,13 +14,17 @@ app.use(cookieSession({name: "user_id", secret: "qwerty"}));
 
 app.set("view engine", "ejs");
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
+// template url database
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
-// global object
+// template users database
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -30,20 +38,7 @@ const users = {
   }
 };
 
-//random 6 string generator
-const generateRandomString = () => Math.random().toString(36).substring(7);
-
-
-//  function that returns the URLs where the userID is equal to the id of the currently logged in user.
-const urlsForUser = (id) => {
-  let urlArr = [];
-  for (let shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === id) {
-      urlArr.push(shortURL);
-    }
-  }
-  return urlArr;
-};
+// ROUTE HANDLERS
 
 app.get("/", (req, res) => {
   if (req.session.user_id) {
@@ -53,22 +48,14 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/urls", (req, res) => {
   //generate new filtered object holding all short URLS matched to the logged in user_id
-  let objURL = {}
-  let urlArr = urlsForUser(req.session.user_id);
+  let objURL = {};
+  let urlArr = urlsForUser(req.session.user_id, urlDatabase);
 
   for (let shortURL in urlDatabase) {
-    for (let j = 0; j<  urlArr.length; j++) {
-      if(shortURL === urlArr[j]) {
+    for (let j = 0; j <  urlArr.length; j++) {
+      if (shortURL === urlArr[j]) {
         objURL[shortURL] = urlDatabase[shortURL];
       }
     }
@@ -76,7 +63,7 @@ app.get("/urls", (req, res) => {
   // pass new filtered object to template file for rendering unique table
   let templateVars = { urls: objURL, user: users[req.session.user_id] };
   if (req.session.user_id) {
-  res.render("urls_index", templateVars);
+    res.render("urls_index", templateVars);
   } else {
     res.sendStatus(403);
   }
@@ -96,23 +83,21 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
-    let templateVars = {
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL,
-      user: users[req.session.user_id]
-    };
-    res.render("urls_show", templateVars);
-    }
-    else res.sendStatus(403);
-  }
-  else res.sendStatus(404);
+      let templateVars = {
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL].longURL,
+        user: users[req.session.user_id]
+      };
+      res.render("urls_show", templateVars);
+    } else res.sendStatus(403);
+  } else res.sendStatus(404);
 });
 
-//when user clicks shortURL, browser redirects to actual webpage by accessing the longURL which stored the full address POSTED in form
+//If user is logged in, redirect to desired website based on shortURL id
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
-  let longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(`http://${longURL}`);
+    let longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(`http://${longURL}`);
   } else {
     res.sendStatus(404);
   }
@@ -133,12 +118,12 @@ app.post("/urls", (req, res) => {
   }
 });
 
-//deletes url
+//deletes url object entry
 app.post("/urls/:shortURL/delete", (req, res) => {
   // check if url's user ID matches cookie's user ID. then delete if yes
   if (urlDatabase[req.params.shortURL].userID === (req.session.user_id)) {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
   } else {
     res.sendStatus(403);
   }
@@ -147,30 +132,28 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //edits and updates new long url
 app.post("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL].userID === (req.session.user_id)) {
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
-  res.redirect("/urls");
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+    res.redirect("/urls");
   } else {
-     res.sendStatus(403);
+    res.sendStatus(403);
   }
 });
 
-//rendering new login template
+//renders new login template
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect("/urls");
   } else {
-  let templateVars = {
-    email: req.body.email,
-    password: req.body.password,
-    user: users[req.session.user_id]
-  };
-  res.render("login", templateVars);
-}
+    let templateVars = {
+      email: req.body.email,
+      password: req.body.password,
+      user: users[req.session.user_id]
+    };
+    res.render("login", templateVars);
+  }
 });
 
-// read body's email and password, find the user that matches those and extract the userID. assign that userID to cookie
-
-// Login function
+// Login event handler
 app.post("/login", (req, res) => {
   // check if email exists
   let user = getUserByEmail(req.body.email, users);
@@ -186,13 +169,13 @@ app.post("/login", (req, res) => {
   }
 });
 
-//logs out user
+// logs out user
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/login");
 });
 
-// show registration page
+// renders registration page
 app.get("/register", (req, res) => {
   if (req.session.user_id) {
     res.redirect("urls");
--- helpers.js
+++ helpers.js
@@ -6,4 +6,20 @@ const getUserByEmail = (email, database) => {
   return false;
 };
 
-module.exports = { getUserByEmail };
+//random 6 string generator
+const generateRandomString = () => Math.random().toString(36).substring(7);
+
+
+//  function that returns the URLs where the userID is equal to the id of the currently logged in user.
+const urlsForUser = (id, database) => {
+  let urlArr = [];
+  for (let shortURL in database) {
+    if (database[shortURL].userID === id) {
+      urlArr.push(shortURL);
+    }
+  }
+  return urlArr;
+};
+
+
+module.exports = { getUserByEmail, urlsForUser, generateRandomString };
